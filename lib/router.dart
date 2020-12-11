c61246cc22b7c5e1310c605c9cef34d454a4ba54
GreyImediary <ser.kushnaryov@gmail.com>
2020-12-11 18:11:00 +0300

    delete introduction

--- lib/router.dart
+++ lib/router.dart
@@ -7,7 +7,6 @@ import 'package:flutter_design_patterns/screens/category/category.dart';
 import 'package:flutter_design_patterns/screens/design_pattern_details/design_pattern_details.dart';
 import 'package:flutter_design_patterns/screens/main_menu/main_menu.dart';
 import 'package:flutter_design_patterns/widgets/factory_method/factory_method.dart';
-import 'package:flutter_design_patterns/widgets/introduction/introduction.dart';
 import 'package:flutter_design_patterns/widgets/singleton/singleton.dart';
 
 class AppRouter {
@@ -22,14 +21,6 @@ class AppRouter {
         return MaterialPageRoute(
           builder: (_) => Category(
             category: category,
-          ),
-        );
-      case _DesignPatternRoutes.introductionRoute:
-        var designPattern = settings.arguments as DesignPattern;
-        return MaterialPageRoute(
-          builder: (_) => DesignPatternDetails(
-            designPattern: designPattern,
-            example: Introduction(),
           ),
         );
 
@@ -59,7 +50,6 @@ class AppRouter {
 }
 
 class _DesignPatternRoutes {
-  static const String introductionRoute = '/introduction';
   static const String singletonRoute = '/singleton';
   static const String factoryMethodRoute = '/factory-method';
 }
